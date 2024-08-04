5ff247fb87ee2c61c820a65763f5b06d8effe204
Aaron Song <74108022+AaronSxng@users.noreply.github.com>
2024-08-04 15:14:43 +0000

    html setup

--- src/test.py
+++ src/test.py
@@ -1,15 +1,15 @@
 import wikipediaapi
     
-wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)', 'en')
 
-page_py = wiki_wiki.page('Python_(programming_language)')
+def exportLink(pagename):
+    wiki_wiki = wikipediaapi.Wikipedia('WikiRacer', 'en')
 
-page_py = wiki_wiki.page('Python_(programming_language)')
-print("Page - Exists: %s" % page_py.exists())
+    page_py = wiki_wiki.page('Python_(programming_language)')
+    print("Page - Exists: %s" % page_py.exists())
 
-def print_links(page):
-        links = page.links
-        for title in sorted(links.keys()):
-            print("%s: %s" % (title, links[title]))
+    def print_links(page):
+            links = page.links
+            for title in sorted(links.keys()):
+                print("%s: %s" % (title, links[title]))
 
-print_links(page_py)
+    print_links(page_py)
