9846954c257f0b35cadaf66b5b7732d63f8bfd42
Aaron Song <74108022+AaronSxng@users.noreply.github.com>
2024-08-04 17:33:26 +0000

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  var graphData = {
    nodes: [{ name: "A" }, { name: "B" }, { name: "C" }, { name: "D" }],
    links: [
-     { source: "A", target: "B" },
+     { source: "A", target: "B", target: "C", target: "D" },
      { source: "B", target: "C" },
      { source: "D", target: "C" }
    ]
