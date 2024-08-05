760a9c56811d5a9b695c4ff1874949a4bde85367
Aaron Song <74108022+AaronSxng@users.noreply.github.com>
2024-08-05 04:34:38 +0000

    Javascript fulls data from wiki api

--- src/scripts.py
+++ src/scripts.py
@@ -1,5 +1,15 @@
+import wikipediaapi
+    
+print("hello world")
+def exportLink(pagename):
+    wiki_wiki = wikipediaapi.Wikipedia('WikiRacer', 'en')
 
+    page_py = wiki_wiki.page(pagename)
+    print("Page - Exists: %s" % page_py.exists())
 
-def main():
-    print("whats up")
+    def print_links(page):
+            links = page.links
+            for title in sorted(links.keys()):
+                print("%s: %s" % (title, links[title]))
 
+    print_links(page_py)
